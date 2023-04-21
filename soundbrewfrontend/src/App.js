466f7b46e312ac0406b076904c9dcdc36fc9c43a
shubhu121 <shubhucsrldelhi@gmail.com>
2023-04-22 02:36:23 +0530

    added signup page

--- soundbrewfrontend/src/App.js
+++ soundbrewfrontend/src/App.js
@@ -1,8 +1,13 @@
+import Profile from "./Profile";
+import Signup from "./components/user/Signup.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the SoundBrew</h1>
+      <Profile></Profile>
+      
+      <Signup></Signup>
     </div>
   );
 }
---
+++ soundbrewfrontend/src/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => {
+
+  return (
+    <div>
+       <h1>Name : Shubehndu</h1>
+
+    </div>
+  )
+}
+
+export default Profile
