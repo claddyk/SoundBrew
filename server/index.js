ae82fc7fda0fff577a9077efb5f08a4220c543b3
SanketTeli <telisanket2002@gmail.com>
2023-04-22 18:53:14 +0530

    done with authentication and middlewares

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ import express from "express";
 import { DATABASE_KEY } from "./config.js";
 import mongoose from "mongoose";
 import { signin, signup } from "./controllers/auth.js";
-
+import cors from 'cors'
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:3000"
+}));
+mongoose.set("strictQuery", false);
 mongoose
   .connect(DATABASE_KEY, {
     useNewUrlParser: true,
@@ -19,7 +25,13 @@ mongoose
 
 app.listen("8000", (req, res) => {
   console.log("server listening");
+ 
+  
 });
 
-app.post("signup", signup);
-app.post("signin",signin);
+app.post("/signup", signup);
+app.get("/signup",(req,res)=>{
+  console.log("welcome")
+  res.send('Welcome to my server!');
+})
+app.post("/signin",signin);
