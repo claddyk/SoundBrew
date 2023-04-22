dbbd1682ce903eae6f7eba7ee9047ad34545b225
madscientists1523@gmail.com <madscientists1523@gmail.com>
2023-04-22 23:13:04 +0530

    New Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,13 @@
-<<<<<<< HEAD
-import Profile from "./Profile";
-import Signup from "./components/user/Signup.js";
+import React from 'react'
+import Signup from './components/user/Signup'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Profile></Profile>
-      <Signup></Signup>
-=======
-import "./App.css";
-import ParticleBackground from "./ParticleBackground";
 
-function App() {
-  return (
     <div>
-      <ParticleBackground />
-      <CenterTitle />
+      <Signup />
     </div>
-  );
+  )
 }
 
-function CenterTitle() {
-  return (
-    <div id="text_div center_all">
-      <div className="center_all">
-        <h1 className="custom-subTitle">Particle JS Integration</h1>
-      </div>
->>>>>>> 0c092d67d45abbb703df7d8d8bd64fe49300b361
-    </div>
-  );
-}
-
-export default App;
+export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
@@ -27,4 +9,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
->>>>>>> 0c092d67d45abbb703df7d8d8bd64fe49300b361
